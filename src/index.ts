0b800343655b234adda6262db5a00a9d8d691d5f
Mason Besmer <masonbesmer@gmail.com>
2023-12-30 23:20:19 -0600

    intentionally mess up formatting

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,7 @@ const client = new SapphireClient({
 		level: LogLevel.Debug
 	},
 	intents: [
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMessageReactions
+	GatewayIntentBits.DirectMessages,GatewayIntentBits.GuildMessages,GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessageReactions
 	],
 	loadMessageCommandListeners: true
 });
