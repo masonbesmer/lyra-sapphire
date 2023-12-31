c2cf11f3353862e36954464db3f6b22d7571eeec
Mason Besmer <masonbesmer@gmail.com>
2023-12-30 22:27:15 -0600

    Remove BeeMovie listener

--- src/listeners/WordTriggers.ts
+++ src/listeners/WordTriggers.ts
@@ -12,13 +12,24 @@ export class UserEvent extends Listener {
 		const msgText = message.content.toLowerCase();
 
 		if (msgText.includes('bee')) {
+			this.container.logger.debug('Replying to "Bee" trigger');
 			message.channel.send('According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don\'t care what humans think is impossible.');
 		}
 		if (msgText.includes('greg')) {
+			this.container.logger.debug('Replying to "Greg" trigger');
 			message.channel.send('did someone say [greg](https://gregtech.overminddl1.com/static/images/logo_gt_site.svg)?');
 		}
 		if (msgText.includes('was')) {
+			this.container.logger.debug('Replying to "Was" trigger');
 			message.channel.send('was was');
+		}
+		if (msgText.includes('bomb')) {
+			this.container.logger.debug('Replying to "Bomb" trigger');
+			message.channel.send(':bomb:');
+		}
+		if (msgText.includes('die')) {
+			this.container.logger.debug('Replying to "Die" trigger');
+			message.channel.send('immediately');
 		}
 		return;
 	}
