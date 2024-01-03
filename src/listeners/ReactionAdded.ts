86c92ce8466eacd63b333753c2d603f63db232eb
Vex <masonbesmer@gmail.com>
2024-01-02 19:32:05 -0600

    Update ReactionAdded.ts
    Testing deployment

--- src/listeners/ReactionAdded.ts
+++ src/listeners/ReactionAdded.ts
@@ -9,7 +9,7 @@ export class UserEvent extends Listener {
 	public override async run(messageReaction: MessageReaction, user: User) {
 		const ID = user.id;
 		return messageReaction.message.channel.send(
-			`<@${ID}>, reactions are known to the state of California to cause cancer and reproductive harm.`
+			`<@${ID}>, reactions are known to the state of California to cause cancer and reproductive harm. Please be careful when using and adding reactions.`
 		);
 	}
 }
