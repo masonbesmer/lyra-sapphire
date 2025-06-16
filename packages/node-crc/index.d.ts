da3350a0e1408a2551515ef9f2489aec6da77e8e
Vex <masonbesmer@gmail.com>
2025-06-15 21:45:47 -0500

    add crc shim

---
+++ packages/node-crc/index.d.ts
@@ -0,0 +1,1 @@
+export function crc(bitLength: number, reflect: boolean, polynomial: number, initLow: number, initHigh: number, xorLow: number, xorHigh: number, refOut: boolean, buffer: Buffer): Buffer;
